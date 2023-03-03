feb991e9208fa81ad834b59a096c5a91fd22c08c
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2023-03-02 22:25:55 -0300

    aula

--- index.php
+++ index.php
@@ -4,7 +4,57 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
   
-    
+  </head>
+ ><body>
+ <div class="container mb-3">
+ <div class="row">
+  <div class="col">
+    <h1> Cadastro novo usuário</h1>
+    <div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Nome" aria-label="First name">
+  </div>
+   <div class="col">
+    <input type="text" class="form-control" placeholder="Senha" aria-label="Last name">
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Sobrenome" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Confirme a senha" aria-label="Last name">
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Email" aria-label="First name">
+  </div>
+
+ <div class="col">
+ 
+  <select class="form-select" aria-label="Default select example">
+  <option selected>Qual estação do ano você nasceu?</option>
+  <option value="1">Verão</option>
+  <option value="2">Inverno</option>
+  <option value="3">Outono</option>
+  <option value="3">Primavera</option>
+</select>
+ </div>
+ </div>
+ </div>
+ 
+<div class="container mb-3">
+<div class="row">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Telefone" aria-label="First name">
+  </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Resposta" aria-label="Last name">
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+<button type="submit" class="btn btn-primary mb-3">Cadastrar</button>
---
+++ parte2/index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <div class="p-3 mb-2 bg-primary text-white">CADASTRO DE CLIENTES</div>
+  </head>
+ ><body>
+ <div class="container mb-5">
+ <div class="row">
+  <div class="col">
+    
+    <h1> Dados cadastrais</h1>
+  </div>
+    <div class="row">
+  <div class="col">
+  <label for="exampleInputEmail1" class="form-label">Nome completo</label>
+    <input type="text" class="form-control" placeholder="" aria-label="First name">
+  </div>
+  
+   <label for="exampleInputEmail1" class="form-label">Endereço</label>
+    <input type="text" class="form-control" placeholder="" aria-label="Last name">
+  </div>
+
+  <div class= "col">
+  
+  
+<label for="exampleInputEmail1" class="form-label">Bairro</label>
+    <input type="text" class="form-control" placeholder="" aria-label="">
+  </div>
+  <div class="col">
+  <label for="exampleInputEmail1" class="form-label">Cidade</label>
+    <input type="text" class="form-control" placeholder="" aria-label="
+    ">
+  </div>
+  <div class="col">
+  <label for="exampleInputEmail1" class="form-label">Estado</label>
+  
+  <select class="form-select" aria-label="Default select example">
+  <option selected></option>
+  <option value="1">SP</option>
+  <option value="2">RJ</option>
+  <option value="3">MG</option>
+  <option value="3">BA</option>
+</select>
+  </div>
+<div class="row">
+<div class= "col">
+  
+  
+<label for="exampleInputEmail1" class="form-label">CEP</label>
+    <input type="text" class="form-control" placeholder="" aria-label="">
+  </div>
+  <div class="col">
+  <label for="exampleInputEmail1" class="form-label">Telefone</label>
+    <input type="text" class="form-control" placeholder="" aria-label="
+    ">
+  </div>
+</div>
+<div class="row">
+  <div class="col">
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3">Observação</textarea>
+ 
+  </div>
+  <div class="row">
+  <div class="col">
+  <div class="list-group">
+  <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+    Comandos
+  </a>
+  
+
+ 
+
